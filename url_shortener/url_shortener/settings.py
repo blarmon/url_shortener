30828c1907ed51499b95d8671a4932c56e5042b0
Siegel <csiegel@dmu.edu>
2018-12-24 11:51:54 -0600

    heroku

--- url_shortener/url_shortener/settings.py
+++ url_shortener/url_shortener/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,5 +127,4 @@ STATIC_URL = '/static/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Configure Django App for Heroku.
-import django_heroku
 django_heroku.settings(locals())
