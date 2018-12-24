51461720e483ded649fcdd90ce45369eac9517e6
Siegel <csiegel@dmu.edu>
2018-12-24 12:01:18 -0600

    moved .git files....

--- url_shortener/url/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.core.validators import URLValidator
-
-
-# Create your models here.
-class URL(models.Model):
-    user_url = models.TextField(validators=[URLValidator()])
-    user_email = models.EmailField(blank=True)
-
